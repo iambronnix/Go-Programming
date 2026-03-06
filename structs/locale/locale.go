b956070a339aea657fa1d36bb3d9aefa300069df
Bronnixx <erickndeto202@gmail.com>
2026-03-06 13:42:40 +0300

    internationalisation and localization concepts

--- structs/locale/locale.go
+++ structs/locale/locale.go
@@ -6,8 +6,8 @@ import (
 )
 
 type locale struct {
-	lang    string
-	country string
+	language string
+	country  string
 }
 
 func getLocale() []locale {
@@ -16,38 +16,37 @@ func getLocale() []locale {
 	var fr_CN locale
 	var fr_FR locale
 	var ru_RU locale
-
-	en_US.lang = "en_US"
+	en_US.language = "en_US"
 	en_US.country = "United States"
-	en_CN.lang = "en_CN"
+	en_CN.language = "en_CN"
 	en_CN.country = "Canada"
-	fr_CN.lang = "fr_CN"
-	fr_CN.country = "Canada"
-	fr_FR.lang = "fr_FR"
+	fr_CN.language = "fr_CN"
+	fr_CN.country = "France"
+	fr_FR.language = "fr_FR"
 	fr_FR.country = "France"
-	ru_RU.lang = "ru_RU"
+	ru_RU.language = "ru_RU"
 	ru_RU.country = "Russia"
-
 	return []locale{en_US, en_CN, fr_CN, fr_FR, ru_RU}
 }
 func main() {
+	parsedArg := struct {
+		language string
+		country  string
+	}{os.Args[1],
+		os.Args[2]}
+
 	validate := getLocale()
 	if len(os.Args) < 2 {
-		fmt.Println("No arguments passed into the terminal")
+		fmt.Println("No language parsed")
 		os.Exit(1)
-	}
-	passedArg := struct {
-		locale
-	}{
-		locale.lang = os.Args[1],
-		locale.country = os.Args[2],
 	}
 	for i := 0; i < len(validate); i++ {
-		if passedArg == validate[i] {
-			fmt.Printf("Locale passed is supported:%+v\n", validate[i])
+
+		if parsedArg == validate[i] {
+			fmt.Printf("%s is supported:%s\n", os.Args[1], validate[i])
 			os.Exit(0)
 		}
-		fmt.Printf("Locale not supported:%+v\n", passedArg)
-		os.Exit(1)
 	}
+	fmt.Printf("locale not supported:\t%s\n", os.Args[1])
+	os.Exit(0)
 }
