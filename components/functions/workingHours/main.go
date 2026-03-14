5ecb9f836af0302c22e7d324fa742eee0b077992
Bronnixx <erickndeto202@gmail.com>
2026-03-14 23:55:53 +0300

    parameter passing and function return activity

---
+++ components/functions/workingHours/main.go
@@ -0,0 +1,43 @@
+package main
+import (
+	"fmt"
+)
+type Employee struct{
+	Id int
+	FirstName string
+	LastName string
+}
+type Developer struct{
+	Individual Employee
+	HourlyRate int
+	WorkWeek [7]int
+}
+type WeekDay int
+const {
+	monday = iota
+	tuesday
+	wednesday
+	thursday
+	friday
+	saturday
+	sunday
+}
+func main(){
+	d := Developer{Individual:Employee{Id: 1, FirstName: "Jayden", LastName:"Mutiri"},HourlyRate:10}
+	d.LogHours(Monday, 8 )
+	d.LogHours(Tuesday, 10)
+	fmt.Println("Hours worked on Monday: ", d.WorkWeek[Monday])
+	fmt.Println("Hours worked on Tuesday: ", d.Workweek[Tuesday])
+	fmt.Printf("Hours worked this week: %d", d.HoursWorked())
+}
+func (d *Developer) LogHours(day WeekDay, hours int){
+	d.WorkWeek[day] = hours
+}
+func (d *Developer) HoursWorked() int {
+	total := 0
+	for _, v:= range d.WorkWeek{
+		total +=
+	}
+	return total
+}
+
