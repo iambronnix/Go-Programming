ac566257f6ff0799c913b45f3d073e5cedd6e7b5
Bronnixx <erickndeto202@gmail.com>
2026-03-14 23:58:29 +0300

    parameter passing and function return

--- components/functions/workingHours/main.go
+++ components/functions/workingHours/main.go
@@ -1,19 +1,22 @@
 package main
+
 import (
 	"fmt"
 )
-type Employee struct{
-	Id int
+
+type Employee struct {
+	Id        int
 	FirstName string
-	LastName string
+	LastName  string
 }
-type Developer struct{
+type Developer struct {
 	Individual Employee
 	HourlyRate int
-	WorkWeek [7]int
+	WorkWeek   [7]int
 }
 type WeekDay int
-const {
+
+const (
 	monday = iota
 	tuesday
 	wednesday
@@ -21,23 +24,23 @@ const {
 	friday
 	saturday
 	sunday
-}
-func main(){
-	d := Developer{Individual:Employee{Id: 1, FirstName: "Jayden", LastName:"Mutiri"},HourlyRate:10}
-	d.LogHours(Monday, 8 )
+)
+
+func main() {
+	d := Developer{Individual: Employee{Id: 1, FirstName: "Jayden", LastName: "Mutiri"}, HourlyRate: 10}
+	d.LogHours(Monday, 8)
 	d.LogHours(Tuesday, 10)
 	fmt.Println("Hours worked on Monday: ", d.WorkWeek[Monday])
 	fmt.Println("Hours worked on Tuesday: ", d.Workweek[Tuesday])
 	fmt.Printf("Hours worked this week: %d", d.HoursWorked())
 }
-func (d *Developer) LogHours(day WeekDay, hours int){
+func (d *Developer) LogHours(day WeekDay, hours int) {
 	d.WorkWeek[day] = hours
 }
 func (d *Developer) HoursWorked() int {
 	total := 0
-	for _, v:= range d.WorkWeek{
-		total +=
+	for _, v := range d.WorkWeek {
+		total += v
 	}
 	return total
 }
-
