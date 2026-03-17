aa1cf1b39688a43553b73eda85bef626c89bb580
Bronnixx <erickndeto202@gmail.com>
2026-03-18 00:37:29 +0300

    basic structure

--- components/functions/anonymousFunctions/closures/structure/main.go
+++ components/functions/anonymousFunctions/closures/structure/main.go
@@ -12,6 +12,6 @@ func main() {
 	}
 	fmt.Println(incrementor()) //first prints 1
 	fmt.Println(incrementor()) // second time prints 2
-	i += 10                    //this increases the value of i by 10
+	i += 10                    //this increases the value of i by 10 ...i can be accessed and changed outside the anonymous function
 	fmt.Println(incrementor()) // prints 12
 }
