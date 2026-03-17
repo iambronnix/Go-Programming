f25c64e27693a3b76e7d398aafc529a24dca3bd9
Bronnixx <erickndeto202@gmail.com>
2026-03-18 01:17:47 +0300

    var i can't be accessed and changed

--- components/functions/anonymousFunctions/closures/structure/solution/main.go
+++ components/functions/anonymousFunctions/closures/structure/solution/main.go
@@ -5,13 +5,13 @@ import (
 )
 
 func main() {
-	increment := increment()
+	increment := increment() //assigns a variable to func() int that gets returned
 	fmt.Println(increment())
 	fmt.Println(increment())
 }
-func increment() func() int {
-	i := 0
-	return func() int {
+func increment() func() int { // increment() is of func() int type
+	i := 0              //only increment() has access to var i
+	return func() int { //return type only increments i...note each call runs the anonymous function code
 		i += 1
 		return i
 	}
