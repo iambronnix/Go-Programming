18a28e830d4e6ecdb6f960802116e3d6288dbc4b
Bronnixx <erickndeto202@gmail.com>
2026-03-21 02:06:55 +0300

    using the concept of anonymousfunctions and naked returns to solve runtime panics

--- functions/nakedReturns/blankreturns/main.go
+++ functions/nakedReturns/blankreturns/main.go
@@ -3,30 +3,24 @@ package main
 import (
 	"errors"
 	"fmt"
-	"os"
 )
 
 func message() (message string, err error) {
-	message = os.Args[1]
+	message = "james"
 	if message == "hi" {
-
 		err = fmt.Errorf("say bye\n") //err is being shadowed during return calling
-		return "", err                //only accesible within the curly brackets
+		return "", err                 //only accesible within the curly brackets
 
 	}
-	err = errors.New("Required string not passed\n")
+	err = errors.New("another string was passed")
 	return "", err
 
 }
-func checkTerminal(message string) (string, error) {
-	if os.Args[1] != message {
-		return "", errors.New("No string was passed into the terminal")
-	}
-	return message, nil
-}
 func main() {
-	_, f := checkTerminal(os.Args[1])
 	_, m := message()
+
+	fmt.Print(m)
+}
 
 	fmt.Print(m)
 	fmt.Println(f)
