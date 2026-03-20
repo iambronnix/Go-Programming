7e3246e2f0fab4410c12ad0d9d2c19c4630a98a8
Bronnixx <erickndeto202@gmail.com>
2026-03-20 23:40:39 +0300

    trying to solve the runtime panic

--- functions/nakedReturns/blankreturns/main.go
+++ functions/nakedReturns/blankreturns/main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"os"
 )
 
 func message() (message string, err error) {
-	message = "hi"
+	message = os.Args[1]
 	if message == "hi" {
-		err := fmt.Errorf("say bye\n") //err is being shadowed during return calling
-		return                         //only accesible within the curly brackets
+
+		err = fmt.Errorf("say bye\n") //err is being shadowed during return calling
+		return "", err                //only accesible within the curly brackets
 
 	}
-	return
+	err = errors.New("Required string not passed\n")
+	return "", err
 
+}
+func checkTerminal(message string) (string, error) {
+	if os.Args[1] != message {
+		return "", errors.New("No string was passed into the terminal")
+	}
+	return message, nil
+}
+func main() {
+	_, f := checkTerminal(os.Args[1])
+	_, m := message()
+
+	fmt.Print(m)
+	fmt.Println(f)
 }
