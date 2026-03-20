d563737d8c756a9b08dbfaa19e8a1502b42a361c
Bronnixx <erickndeto202@gmail.com>
2026-03-20 11:40:21 +0300

    func type passing

--- components/functions/functiontypes/typecalc/main.go
+++ components/functions/functiontypes/typecalc/main.go
@@ -1,17 +1,20 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 type calc func(int, int) string
 
 func main() {
-	calculator(add, 5, 6)
+	calculator(add, 5, 7)
+	calculator(sub, 9, 4)
 }
 func add(i, j int) string {
 	result := i + j
 	return fmt.Sprintf("Added %d + %d = %d", i, j, result)
+}
+func sub(i, j int) string {
+	result := i - j
+	return fmt.Sprintf("Subtracted %d - %d = %d", i, j, result)
 }
 func calculator(f calc, i, j int) {
 	fmt.Println(f(i, j))
