e1a373c81e9d9ab35311f706a5cbcb0c4a0fe789
Bronnixx <erickndeto202@gmail.com>
2026-03-20 16:46:10 +0300

    demostrates how to return functions using anonymous funcs

--- components/functions/returnfuncs/main.go
+++ components/functions/returnfuncs/main.go
@@ -4,10 +4,10 @@ import "fmt"
 
 func main() {
 	v := square(9)
-	fmt.Println(v())
-	fmt.Printf("Type of v: %T", v)
+	fmt.Println(v())                 //calls the function returned by square(9), which calculates the square of 9 and returns 81
+	fmt.Printf("Type of v: %T\n", v) //returns the type of v, which is func() int, indicating that v is a function that takes no arguments and returns an integer
 }
-func square(x int) func() int {
+func square(x int) func() int { //square is a function that takes an integer and returns a function that takes no arguments and returns an integer
 	f := func() int {
 		return x * x
 	}
