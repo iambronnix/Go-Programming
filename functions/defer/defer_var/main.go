502bf3525458e303a8d9b7a99b1967e72ac14de2
iambronnix <erickndeto202@gmail.com>
2026-03-24 11:19:54 +0300

    defer statements in conjuction with variables

--- functions/defer/defer_var/main.go
+++ functions/defer/defer_var/main.go
@@ -7,10 +7,10 @@ import (
 func main() {
 	age := 25
 	name := "Meozy"
-	defer personAge(name, age)
-	age *= 2
+	defer personAge(name, age) //defer statement will execute after the main function finishes, but it will use the value of age at the time of defer statement execution, which is 25.
+	age *= 2                   //double the age after the defer statement
 	fmt.Printf("Age double %d.\n", age)
 }
-func personAge(name string, i int) {
+func personAge(name string, i int) { //this function will be called after the main function finishes, but it will use the value of age at the time of defer statement execution, which is 25.
 	fmt.Printf("%s is %d.\n", name, i)
 }
