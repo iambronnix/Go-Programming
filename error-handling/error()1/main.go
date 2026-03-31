b9f2d2f1f247ef751247f596af91016c92fcb1fc
iambronnix <erickndeto202@gmail.com>
2026-03-31 11:37:01 +0300

    strconv.Atoi() func

--- error-handling/error()1/main.go
+++ error-handling/error()1/main.go
@@ -7,15 +7,16 @@ import (
 
 func main() {
 	v := "10"
-	if s, err := strconv.Atoi(v); err == nil { //strconv.Atoi() returns int and an error
+	if s, err := strconv.Atoi(v); err == nil {
 		fmt.Printf("%T, %v\n", s, s)
-
 	} else {
 		fmt.Println(err)
 	}
-	v = "s2"
-	s, err := strconv.Atoi(v)
+
+	v = "golang254"
+	_, err := strconv.Atoi(v)
 	if err != nil {
-		fmt.Println(s, err)
+		fmt.Println(err)
 	}
+
 }
