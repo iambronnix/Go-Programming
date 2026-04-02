21a8b7faef715d7b86c981c04d92f05db90fa110
iambronnix <erickndeto202@gmail.com>
2026-04-02 17:52:00 +0300

    panic and defer working together

--- panics/panic3/main.go
+++ panics/panic3/main.go
@@ -1,13 +1,13 @@
-package test
+package main
 
 import (
 	"errors"
 	"fmt"
 )
 
-func test() {
+func main() {
 	n := func() {
-		fmt.Println("Defer in test")
+		fmt.Println("Defer in test") //going up the call stack, the next function is the main(), and it's deferred function, n() will execute
 	}
 	defer n()
 	msg := "good-bye"
@@ -15,10 +15,10 @@ func test() {
 }
 func message(msg string) {
 	f := func() {
-		fmt.Println("Defer in message func")
+		fmt.Println("Defer in message func") //deferred f(), runs in the message() function
 	}
 	defer f()
-	if msg == "good-bye" {
+	if msg == "good-bye" { //when panic occurs,message() runs the defer statement within the panicking(), message()
 		panic(errors.New("something went wrong"))
 	}
 
