bb38c30dfbabd7b0b7744ac8b01066dcf3cf1a55
iambronnix <erickndeto202@gmail.com>
2026-04-03 16:05:24 +0300

    recovering from a panic through recovery() func

---
+++ recovery/recover2/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+var (
+	ErrHoulryRate  = errors.New("Invalid hourly rate")
+	ErrHoursWorked = errors.New("Invalid hours worked per week ")
+)
+
+func main() {
+	pay := payDay(100, 25)
+	fmt.Println(pay)
+	pay = payDay(100, 200)
+	fmt.Println(pay)
+	pay = payDay(60, 25)
+	fmt.Println(pay)
+}
+
+func payDay(hoursWorked, hourlyRate int) int {
+	defer func() {
+		if r := recover(); r != nil {
+			if r == ErrHoulryRate {
+				fmt.Printf("hourly rate: %d\nerr: %v\n\n", hourlyRate, r)
+			}
+			if r == ErrHoursWorked {
+				fmt.Printf("hours worked: %d\nerr: %v\n\n", hoursWorked, r)
+			}
+
+		}
+		fmt.Printf("Pay was calculated based on:\nhours worked: %d\nhourly Rate: %d\n", hoursWorked, hourlyRate)
+	}()
+	if hourlyRate < 10 || hourlyRate > 75 {
+		panic(ErrHoulryRate)
+	}
+	if hoursWorked < 0 || hoursWorked > 80 {
+		panic(ErrHoursWorked)
+	}
+	if hoursWorked > 40 {
+		hoursOver := hoursWorked - 40
+		overTime := hoursOver * 2
+		regularPay := hoursWorked * hourlyRate
+		return regularPay + overTime
+	}
+	return hoursWorked * hourlyRate
+}
