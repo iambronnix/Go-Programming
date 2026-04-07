40de095be6c9e8016f8160e51ade30ca9866a4f3
iambronnix <erickndeto202@gmail.com>
2026-04-07 16:13:55 +0300

    implementing interfaces implicitly

--- interfaces/inter-face2/main.go
+++ interfaces/inter-face2/main.go
@@ -17,13 +17,13 @@ func main() {
 		name: "meozy",
 		age:  3,
 	}
-	fmt.Println(d.Speak())
-	fmt.Println(d)
+	fmt.Println(d.Speak()) //calling the Speak method of the cat struct and printing the result
+	fmt.Println(d)         //calling the String method of the cat struct and printing the result
 
 }
-func (c cat) Speak() string {
+func (c cat) Speak() string { //Speak method returns a string
 	return "Purr Meow"
 }
-func (c cat) String() string {
+func (c cat) String() string { //String method returns a string representation of the cat struct
 	return fmt.Sprintf("%v (%v years old)\n", c.name, c.age)
 }
