b7cce85e7e941d381844bf3b8784f2604ac12443
iambronnix <erickndeto202@gmail.com>
2026-04-10 16:26:58 +0300

    implementing interfaces

--- interfaces/inter-face1/main.go
+++ interfaces/inter-face1/main.go
@@ -4,20 +4,20 @@ import (
 	"fmt"
 )
 
-type Speaker interface {
+type Speaker interface { //Speaker interface has a method Speak() that returns a string
 	Speak() string
 }
-type cat struct {
+type cat struct { //cat struct implements the Speaker interface
 }
 
 func main() {
-	c := cat{}
-	fmt.Println(c.Speak())
-	c.Greeting()
+	c := cat{}             //creating an instance of cat struct
+	fmt.Println(c.Speak()) //calling the Speak method of the cat struct and printing the result
+	c.Greeting()           //calling the Greeting method of the cat struct
 }
-func (c cat) Speak() string {
+func (c cat) Speak() string { //Speak method returns a string
 	return "Purr Meow"
 }
-func (c cat) Greeting() {
+func (c cat) Greeting() { //Greeting method prints a message
 	fmt.Println("Meow, Meow!!!!mmmmmeeeeeeooooooowwwww")
 }
