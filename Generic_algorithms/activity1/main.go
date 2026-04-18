1583e40afb0239bd4b6364f84cf149d5df7aa2fa
iambronnix <erickndeto202@gmail.com>
2026-04-18 16:15:54 +0300

    dulicate code pattern trying to refactor

--- Generic_algorithms/activity1/main.go
+++ Generic_algorithms/activity1/main.go
@@ -5,14 +5,28 @@ import (
 	"fmt"
 )
 
+var ErrArray = errors.New("you're passing an empty array ")
+
 func main() {
 	max, _ := findMaxInt([]int{34, 32, 56, 87, 12, 34, 22})
-	fmt.Printf("max interger value:%v\n", max)
+	max2, _ := findMaxFloat([]float64{63, 23, 34, 54, 67, 31})
+	fmt.Printf("max float value:%v\nmax int value:%v\n", max2, max)
 }
 func findMaxInt(nums []int) (int, error) {
 	if len(nums) == 0 {
-		err := errors.New("an empty array")
-		panic(err)
+		panic(ErrArray)
+	}
+	max := nums[0]
+	for _, key := range nums {
+		if max > key {
+			max = key
+		}
+	}
+	return max, ErrArray
+}
+func findMaxFloat(nums []float64) (float64, error) {
+	if len(nums) == 0 {
+		panic(ErrArray)
 
 	}
 	max := nums[0]
@@ -21,5 +35,5 @@ func findMaxInt(nums []int) (int, error) {
 			max = num
 		}
 	}
-	return max, nil
+	return max, ErrArray
 }
