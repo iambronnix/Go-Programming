10e0bfa4ba55a3aefe55697a3aa7567df941558d
iambronnix <erickndeto202@gmail.com>
2026-05-16 12:59:45 +0300

    working with interrupt signal

--- file-systems/signals/activity/main.go
+++ file-systems/signals/activity/main.go
@@ -1,9 +1,11 @@
 package main
-import(
+
+import (
 	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 )
 func main(){
 	sigs := make(chan os.Signal, 1)
@@ -17,11 +19,13 @@ func main(){
 				fmt.Println()
 				fmt.Println("My process has been interrupted. someone might pressed CTRL+C")
 				fmt.Println("Some clean up is occuring")
+				time.Sleep(5 * time.Second)
 				done <- struct{}{}
+				return
 			}
 		}
 	}()
 	fmt.Println("Program is blocked until a signal is caught")
-	done <- struct{}{}
+	<- done
 	fmt.Println("Out of here...I'm done")
 }
