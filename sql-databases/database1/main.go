f71b388506df5b34bd31767b0a5d3eab8c9d6e79
iambronnix <erickndeto202@gmail.com>
2026-05-26 22:35:43 +0300

    initialising a postgres connection

--- sql-databases/database1/main.go
+++ sql-databases/database1/main.go
@@ -1,18 +1,35 @@
 package main
+
 import (
-	"fmt"
 	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	u "github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 func main(){
-	db, err := sql.Open("postgres", "user=bronnix-nerd password=Start123 host=127.0.0.1 port=5432 dbname=postgres sslmode=disable")
+	err := u.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	connStr:= fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)		
+	db, err := sql.Open("postgres", connStr)
 	if err != nil{
-		panic(err)
+		log.Fatal(err)
 	}else{
 		fmt.Println("The connection to the DB was successfully initialised!!!")
 	}
 	connectivity := db.Ping()
 	if connectivity != nil{
-		panic(err)
+		log.Fatal(err)
 	}else{
 		fmt.Println("Good to go!")
 	}
