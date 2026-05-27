ae288c48f28d2691ef65d62422e52b2301a8d714
iambronnix <erickndeto202@gmail.com>
2026-05-27 22:42:48 +0300

    updated init()

--- sql-databases/database2/main.go
+++ sql-databases/database2/main.go
@@ -5,40 +5,82 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	u "github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
-func main(){
+func Init2(){
+	fmt.Println("~~~~~~~~~~Loading things up~~~~~~~~~")
+	time.Sleep(5*time.Second)
+}
+func Init()string{ //does that env loading thing
 	err := u.Load()
 	if err != nil{
 		log.Fatal(err)
 	}
 	dbCreds := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=disable",
 		os.Getenv("DB_USER"),
-	    os.Getenv("DB_PASSWORD"),
-	    os.Getenv("DB_HOST"),
-	    os.Getenv("DB_PORT"),
-	    os.Getenv("DB_NAME"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
 	)
-	db, err := sql.Open("postgres", dbCreds)
+	return dbCreds
+}
+func main(){
+	Init2()
+	createTable(Init())
+	insertTable(Init())
+	
+}
+func createTable(dbCreds string){
+	db, err := sql.Open("postgres",dbCreds)
 	if err != nil{
-		panic(err)
+		log.Fatal(err)
 	}else{
-		fmt.Println("The connection to the DB was successfully initialized!!!")
+		fmt.Println("The connection to the DB was successfully initialised!")
 	}
-	DBCreate := `
-	CREATE TABLE public.test(
+	defer time.Sleep(5 * time.Second)
+	defer func(){//handles panic incase of table exists
+		err := recover()
+		if err != nil{
+			fmt.Println("Error in creating a table")
+		}
+	}()
+	defer db.Close()//closes the db just before exit
+	dbTable := `
+	CREATE TABLE publictest(
 	id int,
 	name character varying COLLATE pg_catalog."default"
 	)
 	WITH(
 	OIDS = FALSE
-	)`
-	_, err = db.Exec(DBCreate)
+	)
+	`
+	_, err = db.Exec(dbTable)
 	if err != nil{
 		panic(err)
 	}else{
-		fmt.Println("The table was successfully created!!!")
+		fmt.Println("The table was successfully created")
+		}
+	
+}
+func insertTable(dbCreds string){
+	db , err := sql.Open("postgres", dbCreds)
+	if err != nil{
+		log.Fatal(err)
+	}else{
+		fmt.Println("The connection to the DB was successfully initialised!!!")
 	}
+	defer db.Close()
+	insert, err := db.Prepare("INSERT INTO publictest VALUES ($1, $2)")
+	if err != nil{
+		panic(err)
+	}
+	_, err = insert.Exec(2,"second")//continue inserting...
+	if err != nil{
+		panic(err)
+	}
+	fmt.Println("The value was successfully inserted!")
 }
