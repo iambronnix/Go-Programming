c93cadd9aed71d33d36d2d75c806240933e81fd5
iambronnix <erickndeto202@gmail.com>
2026-05-28 19:00:36 +0300

    creating and inserting series of values

---
+++ sql-databases/database3/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	u "github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+func Init()string{
+	if err := u.Load();err != nil{
+		log.Fatal(err)
+	}
+	dbCreds := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+	return dbCreds
+	
+}
+func main(){
+	createTable()
+	time.Sleep(5*time.Second)
+	insertTable()
+}
+func createTable(){
+	fmt.Println("Creating db......")
+	dbCreds := Init()
+	db, err := sql.Open("postgres",dbCreds)
+	if err != nil{
+		log.Fatal(err)
+	}
+	dbTable := `
+	CREATE TABLE Numbers(
+	Number int NOT NULL,
+	Property text COLLATE pg_catalog."default" NOT NULL
+	)
+	WITH(
+	OIDS = FALSE
+	)
+	TABLESPACE pg_default;
+	ALTER TABLE Number
+	OWNER to postgres;
+	`
+	defer func(){
+		if err := recover();err !=nil{
+			fmt.Println("error creating table")
+		}
+	}()
+	defer db.Close()
+	_, err = db.Exec(dbTable)
+	if err != nil{
+		panic(err)
+	}else{
+		fmt.Println("The table called Number was successfully created")
+	}
+	
+}
+func insertTable(){
+	fmt.Println("landed on insertTable() func")//used as a marker for debugging only
+	dbCreds := Init()
+	db, err := sql.Open("postgres", dbCreds)
+	if err != nil{
+		log.Fatal(err)
+	}
+	if err = db.Ping();err != nil{
+		fmt.Println("Connection to the DB lost")
+		log.Fatal(err)
+	}
+	insert , insertErr := db.Prepare("INSERT INTO Number VALUES($1, $2)")
+	if insertErr != nil{
+		panic(insertErr)
+	}
+	var prop string
+	for i := 0; i < 100; i++{
+		if i %2 == 0{
+			prop = "Even"
+		}else{
+			prop = "Odd"
+		}
+		_, err = insert.Exec(i, prop)
+		if err == nil{
+		fmt.Println("The number:",i,"is:",prop)
+		}
+	}
+
+}
