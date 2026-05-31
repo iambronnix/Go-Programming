bb02167080cfae8e0d9fa1dd85f0e99b3049190d
iambronnix <erickndeto202@gmail.com>
2026-05-31 22:52:29 +0300

    fixed return values

--- file-systems/files/activity6/main.go
+++ file-systems/files/activity6/main.go
@@ -11,20 +11,20 @@ var(
 	ErrWorkingFileNotFound = errors.New("Working file doesn't exist")
 )
 func main(){
-	message := "Notes:\n1.Get better at coding.\n"
-	err := createBackup(createFile(message))
+	err := createBackup(createFile())
+	log.SetFlags(log.Llongfile)
 	if err != nil{
 		log.Fatal(err)
 	}
-	workFile,_ := createFile(message)
+	workFile,_ := createFile()
 	for i := 1; i <=10; i++{
-		note  := message +" " + strconv.Itoa(i)
+		note  := " " + strconv.Itoa(i)
 		err := addNotes(workFile, note)
 		if err != nil{
 			log.Fatal(err)
 		}
 	}
-	
+
 }
 func addNotes(workFile, notes string)error{
 	notes += "\n"
@@ -39,12 +39,12 @@ func addNotes(workFile, notes string)error{
 	defer f.Close()
 	if _, err := f.Write([]byte(notes)); err != nil{
 		log.Panic(err)
-	} 
+	}
 	return nil
 }
 func createBackup(filesCreated ...string)error{
-	workFile := filesCreated[1]
-	backupFile := filesCreated[2]
+	workFile := filesCreated[0]
+	backupFile := filesCreated[1]
 	_, err := os.Stat(workFile)
 	   if err != nil{
 		if os.IsNotExist(err){
@@ -57,7 +57,7 @@ func createBackup(filesCreated ...string)error{
 	}
 	content, err := io.ReadAll(working)
 	if err != nil{
-		return err 
+		return err
 	}
 	err  = os.WriteFile(backupFile, content, 0644)
 	if err != nil{
@@ -65,15 +65,17 @@ func createBackup(filesCreated ...string)error{
 	}
 	return nil
 }
-func createFile(message string)(string, string){
-	files := [2]string{os.Args[1],os.Args[2]}
+func createFile()(string, string){
+	message := "Notes\n1.Get better at coding\n"
+	 files := [2]string{os.Args[1], os.Args[2]}    
+       
 	for _, i := range files{
 		os.Create(i)
-		err := os.WriteFile(files[1],[]byte(message),0644)
+		err := os.WriteFile(files[0],[]byte(message),0644)
 		if err!=nil{
 			log.Fatal(err)
 		}
-	
+
 	}
 	return files[0], files[1]
 }
