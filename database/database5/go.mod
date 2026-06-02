8a576ea31d1368bf3c152100643679690251875e
iambronnix <erickndeto202@gmail.com>
2026-06-02 20:48:15 +0300

    added delete func

---
+++ database/database5/go.mod
@@ -0,0 +1,10 @@
+module main.go
+
+go 1.26.3
+
+require (
+	github.com/iambronnix/db v1.0.2
+	github.com/lib/pq v1.12.3
+)
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- database/database5/main.go
+++ database/database5/main.go
@@ -4,12 +4,53 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-
+	"time"
+	_ "github.com/lib/pq"
 	d "github.com/iambronnix/db"
 )
+func main(){
+	fmt.Println("Initialising the database creds")//just for fun
+	time.Sleep(5 * time.Second)//also this
+	updateTable()//updates specific data from the database
+	deleteTable()//deletes specific data from the database
+}
 	
 func updateTable(){
 	dbCreds, err := d.Config()
+	if err != nil{
+		log.Fatal(err)
+	}
+	fmt.Println(dbCreds)
+	db, err := sql.Open("postgres", dbCreds)
+	if err != nil{
+		log.Fatal(err)
+	}
+	defer db.Close()
+	fmt.Printf("Connection to db was successfully initialised!!!\n")
+	updateStatement := `
+	UPDATE test(
+	SET name = $1
+	WHERE id = $2
+	)
+	`
+	updateResult, updateResultErr := db.Exec(updateStatement)
+	if err = db.Ping(); err != nil{
+		fmt.Println("......Connection lost.....")
+	}else{
+		fmt.Println("database still connected...good to go")
+	}
+	if updateResultErr!= nil{
+		panic(updateResultErr)
+	}
+	updatedRecords, updatedRecordsErr := updateResult.RowsAffected()
+	if updatedRecordsErr != nil {
+		panic(updatedRecordsErr)
+	}
+	fmt.Println("Number of records updated: ", updatedRecords)
+	
+}
+func deleteTable(){
+	dbCreds, err  := d.Config()
 	if err != nil{
 		log.Fatal(err)
 	}
@@ -18,6 +59,17 @@ func updateTable(){
 		log.Fatal(err)
 	}
 	defer db.Close()
-	fmt.Printf("Connection to db was successfully initialised!!!")
-	
+	deleteStatement := `
+	DELETE FROM test
+	WHERE id = $1
+	`
+	deleteResult, deleteResultErr := db.Exec(deleteStatement,2)
+	if deleteResultErr != nil{
+		panic(deleteResultErr)
+	}
+	deletedRecords, deletedRecordsErr := deleteResult.RowsAffected()
+	if deletedRecordsErr != nil{
+		panic(deletedRecordsErr)
+	}
+	fmt.Println("Number of records deleted: ",deletedRecords)
 }
