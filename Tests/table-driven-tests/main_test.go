cbb4c6462326c66e9c031ccb05ae3cec39ba78a0
iambronnix <erickndeto202@gmail.com>
2026-06-15 13:41:46 +0300

    developing unit tests with tables

---
+++ Tests/table-driven-tests/main_test.go
@@ -0,0 +1,46 @@
+package main
+import (
+	"testing"
+	"gotest.tools/assert"
+	
+)
+func add(x,y int)int{
+	return x + y
+}
+func TestAdd(t *testing.T) {
+	tests := []string{
+		name string
+		inputs []int
+		want int
+	}{
+	 {
+	name : "Test Case 1",
+	inputs: []int {5, 6},
+	want: 11,
+	},
+	 {
+	name : "Test Case 2",
+	inputs : []int {11, 7},
+	want: 18,
+	},
+	 {
+	name : "Test Case 3",
+	inputs: []int{1, 8},
+	want: 9,
+	},
+	{
+	name: "Test Case 4( "intentional failure!!")",
+	inputs: []int{2,3},
+	want:0,//This should be 5, intentionally incorrect to demostrate failure
+	},
+	}
+
+	for _, test := range tests {
+		test := test 
+        t.Run(test.name, func(t *testing.T)){		
+		got := add(test.inputs[0],test.inputs[1])
+		assert.Equal(t, test.want, got)
+	}
+	
+	
+}
