3f5b2ed2f6472e8af2e226e7d0c606982be18c46
iambronnix <erickndeto202@gmail.com>
2026-06-15 13:44:14 +0300

    added comments

--- concurrency/channels/channel6/main.go
+++ concurrency/channels/channel6/main.go
@@ -1,11 +1,14 @@
 package main
 
-import "log"
+import (
+	"log"
+	"sync"
+)
 func main(){
 	res := sum(100,1,100)
 	log.Println(res)
 }
-func worker(in chan int, out chan int){
+func worker(in chan int, out chan int){//worker pod 
 	sum := 0
 	for i := range in{//a loop that ranges over the channel
 		sum += i
@@ -14,15 +17,21 @@ func worker(in chan int, out chan int){
 	<- in
 }
 func sum(workers,from, to int)int{
+	wg := &sync.WaitGroup{}
 	out:=make(chan int, workers)
-	in:=make(chan int,4)
-	for i := 0; i <= workers; i++{//runs requested number of workers
+	in:=make(chan int,5)
+	go func(){
+		for i := 0; i <= workers; i++{//runs requested number of workers
+		wg.Add(i)
 		go worker(in,out)
 	}
+	wg.Done()
 	for i := from; i <=to; i++{
 		in <- i//sends all numbers to be summed to the in channel,which will distribute the numbers across all Goroutines.
 	}
 	close(in)//notifies worker func numbers to sum are finished
+	}()
+	wg.Wait()
 	sum := 0
 	for i := 0 ; i <= workers; i ++{//performs sum of the partials
 		sum += <- out
