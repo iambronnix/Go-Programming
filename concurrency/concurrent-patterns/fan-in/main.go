c4294932b4e1c8779120b30845cea05f84358944
iambronnix <erickndeto202@gmail.com>
2026-07-02 19:57:02 +0300

    fan-in concurrency pattern

---
+++ concurrency/concurrent-patterns/fan-in/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "sync"
+
+func fanIn(channels ...chan int){
+	 var wg sync.WaitGroup
+		muxedStream := make(chan int)
+		output := func(c chan int){//sub-worker to get values from each channel
+			for n := range c { //range over the channel
+				muxedStream <- n
+			}
+			wg.Done()
+		}
+		wg.Add(len(channels))
+		for _, channel := range channels{//ranges the pool of channels
+			go output(channel)//creates sub-workers
+		}
+		go func(){
+			wg.Wait()
+		}()
+	} 
