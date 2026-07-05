17675ff8f73b79dc8d94b3f04bec69528380317a
iambronnix <erickndeto202@gmail.com>
2026-07-06 00:25:55 +0300

    method declaration

--- methods/declaration/main.go
+++ methods/declaration/main.go
@@ -3,15 +3,24 @@ package main
 import (
 	"fmt"
 	"math"
+
 )
 type Point struct{
 	x,y float64	
 }
+type Path []Point//a Path is journey connecting the points with straight lines
 func main(){
 	p := Point{1,2}
 	q := Point{4,6}
+	perim := Path{
+		{1,1},
+		{5,1},
+		{5,4},
+		{1,1},
+	}
 	fmt.Println(Distance(p,q))//function call
 	fmt.Println(p.Distance(q))//method call
+	fmt.Println(perim.Distance())
 }
 func Distance(p, q Point)float64{
 	return math.Hypot(q.x - p.x, q.y- p.y)
@@ -19,4 +28,13 @@ func Distance(p, q Point)float64{
 //method of the Point type 
 func (p *Point)Distance(q Point)float64{
 	return math.Hypot(q.x-p.x,q.y-p.y)
+}
+func(path Path) Distance() float64{
+	sum := 0.0
+	for i := range path{
+		if i > 0{
+			sum += path[i-1].Distance(path[i])
+		}
+	}
+	return sum
 }
